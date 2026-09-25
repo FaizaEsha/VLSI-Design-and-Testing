50d47f994ab6ef965096e4ac63fd658b4fadb0a1
Faiza Ahmed Esha <140197867+Faiza-ahmed-esha@users.noreply.github.com>
2026-09-25 18:11:13 +0600

    8 bit full adder completed

---
+++ full_adder_8bit/full_adder_8bit/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "secureip" -o "/home/ise/CSE_450_Xilinx/Faiza_VLSI/adder_8bit_tb_isim_beh.exe" -prj "/home/ise/CSE_450_Xilinx/Faiza_VLSI/adder_8bit_tb_beh.prj" "work.adder_8bit_tb" 
